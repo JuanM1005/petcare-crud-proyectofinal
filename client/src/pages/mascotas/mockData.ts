64751890a6f3ae1fe6013e87737c3bc84926fecd
JuanM1005 <juan.aguirre5548@alumnos.udg.mx>
2026-04-29 12:52:44 -0600

    feat: añadir mockData para pruebas en mascotas

---
+++ client/src/pages/mascotas/mockData.ts
@@ -0,0 +1,5 @@
+// Estos datos simulan lo que después vendrá de la API.
+// Los separamos en su propio archivo para que sea fácil
+// eliminarlos cuando conectemos el backend.
+
+import moduleName from '';
